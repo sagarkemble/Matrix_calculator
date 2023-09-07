8f3a16e5eafb46dcb80d0407bc457b75e88c974a
Sagar kemble <131292159+sagarkemble@users.noreply.github.com>
2023-09-07 18:48:15 +0530

    Update inverse_of_matrix.c

--- Subcode/inverse_of_matrix.c
+++ Subcode/inverse_of_matrix.c
@@ -79,7 +79,7 @@ for (int  i = 0; i < 3; i++)
 
 
 
-printf("YOU ENTERED MATRIX IS \n");
+printf("INPUT MATRIX IS \n");
         for (int  i = 0; i < 3; i++)
     {
         for (int  j = 0; j < 3; j++)
@@ -90,7 +90,7 @@ printf("YOU ENTERED MATRIX IS \n");
         printf("\n");
     }
 
-    printf("INPUT MATRIX IS \n");
+    printf("ADJOINT MATRIX IS \n");
         for (int  i = 0; i < 3; i++)
     {
         for (int  j = 0; j < 3; j++)
